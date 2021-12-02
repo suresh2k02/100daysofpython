c1f64bafd04e912c4408738f968543dfe9c07cfb
suresh2k02 <suresh2k02@gmail.com>
2021-12-02 11:23:01 +0530

    day4

---
+++ day4/task2.py
@@ -0,0 +1,17 @@
+
+import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+
+namesAsCSV = input("Give me everybody's names, seperated by a comma. ")
+names = namesAsCSV.split(", ")
+ 
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today!")
