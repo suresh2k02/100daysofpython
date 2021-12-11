5e8268f722f828c2c6596a38e1dffefc526c45bf
suresh2k02 <suresh2k02@gmail.com>
2021-12-11 10:32:00 +0530

    tress

--- trees/main.py
+++ trees/main.py
@@ -10,3 +10,28 @@ root.childleft = Node('B')
 root.childright = Node('C')
 root.childleft.childleft = Node('D')
 root.childleft.childright = Node('E')
+
+
+
+def InOrd(root):
+    if root:
+        InOrd(root.childleft)
+        print(root.nodedata)
+        InOrd(root.childright)
+InOrd(root)
+
+
+def PreOrd(root):
+    if root:
+        print(root.nodedata)
+        PreOrd(root.childleft)
+        PreOrd(root.childright)
+PreOrd(root)
+
+
+def PostOrd(root):
+    if root:
+        PostOrd(root.childleft)
+        PostOrd(root.childright)
+        print(root.nodedata)
+PostOrd(root)
